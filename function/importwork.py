fd3dcfcf8f05bfb2252bd520fc3c45e0d6c9a226
BhaveshManekar <bhaveshmanekar5544@gmail.com>
2026-02-14 18:03:05 +0530

    importwork.py

---
+++ function/importwork.py
@@ -0,0 +1,13 @@
+# import math
+# result=math.sqrt(167)
+# print(result)
+
+# from math import sqrt,pi
+# result=sqrt(9)*pi
+# print(result)
+
+# to import all the element into the math we use *
+
+from math import *
+result=sqrt(9)*pi
+print(result)
