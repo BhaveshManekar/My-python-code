783e018e1149205ae0adc6c0611f44671f765fe2
BhaveshManekar <bhaveshmanekar5544@gmail.com>
2026-02-17 19:14:23 +0530

    demo.pyimportwork.py

--- function/importwork.py
+++ function/importwork.py
@@ -1,13 +1,27 @@
 # import math
-# result=math.sqrt(167)
+# result=math.sqrt(169)
 # print(result)
 
 # from math import sqrt,pi
-# result=sqrt(9)*pi
+# result=sqrt(144)*pi
 # print(result)
 
-# to import all the element into the math we use *
+# # to import all the element into the math we use *
 
-from math import *
-result=sqrt(9)*pi
-print(result)
+# from math import *
+# result=sqrt(169)*pi
+# print(result)
+
+# # # "as" keyword
+# import math as m
+
+# result = m.sqrt(196) * m.pi
+# print(result)
+
+# to show what math contain
+# import math
+# print(dir(math))
+
+from demo import welcome, bhavesh
+welcome()
+print(demo)
